7f34183ca7637a4f43942d570bf73e718274905c
Drizzy_122 <C21364936@mytudublin.ie>
2023-10-09 22:27:22 +0100

    changed a few things

---
+++ My project/Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Camera cam;
+    private float xRotation = 0f;
+
+    public float xSensitivity = 30f;
+    public float ySensitivity = 30f;
+
+    void Start()
+    {
+        // Lock the cursor at the beginning
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public void ProcessLook(Vector2 input)
+    {
+        float mouseX = input.x;
+        float mouseY = input.y;
+        //calculate camera rotation for looking up and down
+        xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        //Apply thus to our camera transform
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        //rotate player to look left and right
+        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
+    }
+}
